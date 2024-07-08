126b3f93dedc798c2a59e963569fa8b2370e3f01
aindrelasaha <aindrelasaha22p@gmail.com>
2024-07-08 22:26:24 +0530

    day 11/100

--- week1/main.js
+++ week1/main.js
@@ -1,2 +1,44 @@
-console.log("hello World");
-console.log(a); 
+// console.log("hello World");
+// console.log(a); 
+
+// let a=1;
+// a=2;
+// console.log(a);  
+// console.log("Hi");
+
+// let firstName="Pritha";
+// let age=19;
+// let isMarried =false;
+
+// console.log("this person name is " + firstName + " and thier age is "+ age)
+
+// //or
+
+// console.log("This person name is")
+// console.log(firstName)
+
+// if(isMarried==true){
+//     console.log(firstName + " is not married");
+// }
+
+// else{
+//     console.log(firstName + " is married");
+// }
+
+//let, var, const, if, else, for
+
+// let answer =0;
+// for(let i=0;i<+100;i++) {
+//     answer=answer+i;
+// }
+// console.log(answer);
+
+
+// // Q. WAP to find even numbers in an array
+
+// const ages=[23 , 56, 17, 11,];
+// for(let i=0;i<ages.length;i++){
+//     if(ages[i]%2==0){
+//         console.log(ages[i]);
+//     }
+// }
